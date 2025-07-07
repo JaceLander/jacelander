0dc672505137c7c6b9d26759023686d85803589d
JaceLander <jacethelander@gmail.com>
2025-07-07 15:11:21 -0400

    more stuff

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import linkedIn from './linkedIn.webp';
 import resume from './resume.png';
 import gitHub from './github logo.png';
-import cLogo from './C Logo.png';
 import cSharpLogo from './Csharp_Logo.png';
 import cssLogo from './css logo.png';
 import gitLogo from './git logo.png';
@@ -11,37 +10,73 @@ import javaScriptLogo from './javascript logo.png';
 import mySQLLogo from './mysql logo.webp';
 import reactLogo from './react logo.png';
 import springbootLogo from './springboot logo.png';
-import jacePic1 from './JacePic1.png';
 import jacePic2 from './JacePic2.png';
 import jaceHeadshot from './jaceheadshot.png'
-import logoThing from './logo.png';
 import mgLogo from './mglogo.png';
+import nflscreenshot from './nfldlescreenshot.png';
+import wordWarden from './WordWarden.png';
+import BeaconLogin from './BeaconLogin.png';
+import BeaconDB from './BeaconDB.png';
 import './App.css';
+import './Experience.css';
+import './Skills.css';
+import './Projects.css';
 // import { Linking } from 'react-native'
 import { useEffect, useRef, useState} from 'react';
 
 
 
 function App() {
+const [proj1Visible, setproj1Visible] = useState(false);
+const [proj2Visible, setproj2Visible] = useState(false);
+const [proj3Visible, setproj3Visible] = useState(false);
+const [proj4Visible, setproj4Visible] = useState(false);
+
+function proj1Button(){
+  setproj1Visible(true);
+  setproj2Visible(false);
+  setproj3Visible(false);
+  setproj4Visible(false);
+}
+
+function proj2Button(){
+  setproj2Visible(true);
+  setproj1Visible(false);
+  setproj3Visible(false);
+  setproj4Visible(false);
+}
+
+function proj3Button(){
+  setproj3Visible(true);
+  setproj1Visible(false);
+  setproj2Visible(false);
+  setproj4Visible(false);
+}
+
+function proj4Button(){
+  setproj4Visible(true);
+  setproj1Visible(false);
+  setproj2Visible(false);
+  setproj3Visible(false);
+}
+
   return (
     
   <div className="App">
         <div className = "pad"></div>
         <div className="App-header">
-        {/* <img src={logoThing} className="logo-" alt="logo"/> */}
         <img src={linkedIn} className="LI-logo" alt="logo" onClick={linkedInClick} />
         <img src={gitHub} className="gitHub-logo" alt="logo" onClick={gitHubButtonClick} />
         <img src={resume} className="resume-logo" alt="logo" onClick={resumeButtonClick} />
-        {/* <ProjectsButton></ProjectsButton> */}
       </div>
-      <BigDiv className="App-Main bgtint" >
+      <BigDiv className="App-Main bgtint">
       <div className='TextDiv'>
       <img src={jaceHeadshot} className="headshot" alt=""/>
       <p className='Title'>Jace Lander</p>
       <p className='element-text'> 
       Full stack software developer from Olmsted Falls, OH </p>
       <p className='element-text'>
-      Student at Baldwin Wallance University </p>
+      Student at Baldwin Wallace University </p>
       </div>
       </BigDiv>
       <BigDiv>
@@ -92,35 +127,95 @@ function App() {
       </div>
      </div>
       </div>
-      <div>
-      <p className='Title center experience'>Experience</p>
-      <div className='experience-container'>
-        <div className='row'>
-            <img src={mgLogo} className="mgLogo"/>
-          <div className='column-exp'>
-            <p className="experience-text white"> Mission Guatemala Medical Records Database Software</p>
-            <p className="experience-text subtext white"> Baldwin Wallace University</p>
-            <p className="experience-text subtext white"> 2024-current</p>
-            <p className="experience-text subtext exp-subtext white"> Worked on a multi-phase, multi-year project developing 
-            a fullstack database software for a rural clinic in Guatemala using various industry standard technologies
-            such as C# and SQL as part of a larger team.</p>
-        </div>
-        </div>
+        <div className='big-column'>
+          <p className='Title center experience'>Experience</p>
+            <div className='experience-container'>
+              <div className='row experience'>
+                  <img src={mgLogo} className="mgLogo"/>
+                <div>
+                  <p className="experience-text title white"> Mission Guatemala Medical Records Database Software</p>
+                <div className='row org-time'>
+                  <div className="experience-text organization white"> Baldwin Wallace University</div>
+                  <div className='timeorggap'></div>
+                  <div className="experience-text time white"> 2024-current</div>
+                </div>
+                  <p className="experience-text white subtext"> Worked on a multi-phase, multi-year project developing 
+                  a fullstack database software for a rural clinic in Guatemala using various industry standard technologies
+                  such as C# and SQL as part of a larger team.</p>
+              </div>
+          </div>
      </div>
       </div>
       </BigDiv>
-      <BigDiv className="App-Main bgtint">
-      <img src={jacePic1} className="element-pic" alt=""/>
-         <div className='TextDiv'> <p className='Title'>Experience</p> 
-         <p className='element-text'>My experience in software engineering has led me to learn many programming languages, such as C, C#, R, Java, JavaScript, and have gained experience utilizing
-          other tools and frameworks like Git, React, SQL and Balsamiq.  </p>
-          </div> 
+      <BigDiv >
+      <div className='big-div'>
+        <p className='Title center experience'>Projects</p>
+          <div className='projects-container'>
+              <div className='projects-buttons'>
+                    <button className={`project-button font ${proj1Visible ? "selected" : ""}`} 
+                    onClick={proj1Button}>NFLdle</button>
+                    <button className={`project-button font ${proj2Visible ? "selected" : ""}`}
+                    onClick={proj2Button}>Word Warden</button>
+                    <button className={`project-button font ${proj3Visible ? "selected" : ""}`}
+                    onClick={proj3Button}>Beacon</button>
+                    <button className={`project-button font ${proj4Visible ? "selected" : ""}`}
+                    onClick={proj4Button}>This Website</button>
+              </div>
+            <div className='projects-seperator'></div>
+            <div className={`project-preview ${proj1Visible ? "toggled" : ""}`}>
+              <div className='column'>
+                {/* <img src={nfliphonescreenshot} className='project-image'></img> */}
+                <img src={nflscreenshot} className='project-image' style={{height: "15vw",
+                width:"30vw", margin:"auto", paddingTop:"1vw"}}></img>
+                <div style={{width:"45vw", padding:"1vw", textAlign:"center", alignSelf:"center"}} className='font'>NFLdle is a front end react app utilizing sportsData.io's 
+                    RESTful API and deployed with cloudflare. The application utilizes live-rendered
+                    elements in response to user input and fetches data in such a way to minimize API calls 
+                    and maximize performance. </div>
+              </div>
+            </div>
+            <div className={`project-preview ${proj2Visible ? "toggled" : ""}`}>
+            <div className='column'>
+                <img src={wordWarden} className='project-image' style={{height: "15vw",
+                width:"auto", margin:"auto", paddingTop:"1vw"}}></img>
+                <div style={{width:"45vw", padding:"1vw", textAlign:"center" , alignSelf:"center"}} className='font'>Word Warden is a fullstack project with the backend &
+                database handled using supabase. It was released with feedback on how to improve
+                it and with many bugs that had to be quickly handled, giving lots of quick experience
+                on deploying a web app. </div>
+              </div>
+            </div>
+            <div className={`project-preview ${proj3Visible ? "toggled" : ""}`}>
+            <div className='column'>
+              <div className='row'>
+                <img src={BeaconLogin} className='project-image' style={{height: "15vw",
+                width:"auto", margin:"auto", paddingTop:"1vw"}}></img>
+                <img src={BeaconDB} className='project-image' style={{height: "15vw",
+                width:"auto", margin:"auto", paddingTop:"1vw"}}></img>
+              </div>
+                <div style={{width:"45vw", padding:"1vw", textAlign:"center", alignSelf:"center"}} className='font'>Beacon is a work-in-progress
+                full-stack application dedicated to reviewing games, building a backlog and sharing with friends. 
+                The database is using MySQL, backend developed with Java and SpringBoot, and frontend in React. This project
+                is hoped to be released sometime early 2026.</div>
+              </div>
+            </div>
+            <div className={`project-preview ${proj4Visible ? "toggled" : ""}`}>
+            <div className='column'>
+                <div style={{width:"45vw", padding:"1vw", textAlign:"center", alignSelf:"center"}} className='font'>Always improving and always changing, this Website
+                is an evolving display of my current state in the ever-evolving field of Software development. Come back in a month
+                and I am sure to have improved this site in some way!</div>
+              </div>
+            </div>
+            
+          </div>
+
+      </div>
       </BigDiv>
 
 
-          <BigDiv className="App-Main bgtint">
-          <div className='TextDiv'> <p className='Title'>Beyond SE</p><p className='element-text'> Beyond my software engineering experience, I have had experience in leadership and professional development.
-          I am the president of the Baldwin Wallace Rugby club where I have set up group events and manage the official instagram. </p>
+          <BigDiv className="App-Main bgtint" >
+          <div className='TextDiv'> <p className='Title'>A Little More About Me</p><p className='element-text'> Beyond my software engineering experience, I have had experience in leadership and professional development.
+          I am the president of the Baldwin Wallace Rugby club where I have set up group events and manage the official instagram. I am currently slated
+          to graduate in spring of 2026, and have a 3.75 GPA. I have a genuine excitement for software and what it has the capability to do, so if any of this 
+          seems interesting to you please feel free to reach out!</p>
           </div>
           <img src={jacePic2} className="element-pic" alt=""/>
           </BigDiv>
@@ -166,7 +261,7 @@ function BigDiv({children}) {
                 setIsVisible(false); 
               }
           },
-          { threshold: 0.9 }
+          { threshold: 0.65 }
       );
 
       if (divRef.current) observer.observe(divRef.current);
